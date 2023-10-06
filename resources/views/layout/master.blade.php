9d48dbf59944dc7ffd06d1b1df21aa5503d43e15
thomasbrian4545 <thomasbrian4545@gmail.com>
2023-10-07 00:21:23 +0700

    Ambil session user ke dashboard

--- resources/views/layout/master.blade.php
+++ resources/views/layout/master.blade.php
@@ -45,7 +45,7 @@
             <a href="#" class="brand-link">
                 <img src="../../dist/img/AdminLTELogo.png" alt="AdminLTE Logo"
                     class="brand-image img-circle elevation-3" style="opacity: .8">
-                <span class="brand-text font-weight-light">SIMKU</span>
+                <span class="brand-text font-weight-light">{{ config('app.name') }}</span>
             </a>
 
             <!-- Sidebar -->
@@ -56,7 +56,7 @@
                         <img src="../../dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
                     </div>
                     <div class="info">
-                        <a href="#" class="d-block">Alexander Pierce</a>
+                        <a href="#" class="d-block">{{ auth()->user()->fullname }}</a>
                     </div>
                 </div>
 
