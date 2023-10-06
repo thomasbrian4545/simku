9b3cf163c74037319aa1acd848b80488f5ca7fe8
thomasbrian4545 <thomasbrian4545@gmail.com>
2023-10-07 00:00:44 +0700

    Membuat halaman dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,1 +1,25 @@
-HALO SELAMAT DATANG
+@extends('layout.master')
+@section('title', 'Dashboard')
+@section('content')
+    <!-- Content Header (Page header) -->
+    <section class="content-header">
+        <div class="container-fluid">
+            <div class="row mb-2">
+                <div class="col-sm-6">
+                    <h1>Dashboard</h1>
+                </div>
+                <div class="col-sm-6">
+                    <ol class="breadcrumb float-sm-right">
+                        <li class="breadcrumb-item"><a href="#">Dashboard</a></li>
+                    </ol>
+                </div>
+            </div>
+        </div><!-- /.container-fluid -->
+    </section>
+
+    <!-- Main content -->
+    <section class="content">
+        <p>Selamat datang, {{ auth()->user() }}</p>
+    </section>
+    <!-- /.content -->
+@endsection
