0030245e24b6c158eb20e7b0936eb5b3e8bbba6a
thomasbrian4545 <thomasbrian4545@gmail.com>
2023-10-15 15:36:38 +0700

    Validasi duplikasi data NIM mahasiswa

--- app/Http/Controllers/MahasiswaController.php
+++ app/Http/Controllers/MahasiswaController.php
@@ -30,7 +30,7 @@ class MahasiswaController extends Controller
     public function store(Request $request)
     {
         $validateData = $request->validate([
-            'nim' => 'required|size:8',
+            'nim' => 'required|size:8|unique:mahasiswas',
             'nama_lengkap' => 'required|min:3|max:50',
             'jenis_kelamin' => 'required|in:P,L',
             'jurusan' => 'required',
